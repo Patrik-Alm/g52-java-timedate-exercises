b04c857d2a3d4b4ea091375fe4049c81413c5989
Patrik Alm <almpatrik@yahoo.se>
2024-10-15 17:13:52 +0200

    Time exercise 8

--- src/main/java/io/github/patrikalm/Main.java
+++ src/main/java/io/github/patrikalm/Main.java
@@ -1,32 +1,19 @@
 package io.github.patrikalm;
 
-import java.time.DayOfWeek;
+
 import java.time.LocalDate;
-import java.time.Month;
 import java.time.Period;
-import java.time.chrono.ChronoLocalDate;
-import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
 
-        LocalDate myBirthdate = LocalDate.parse("1972-02-19");
+        Period period = Period.of(4,7,29);
 
-        LocalDate date = LocalDate.now();
+        LocalDate today = LocalDate.now();
 
-        date = date.plusYears(10).minusMonths(10);
+        LocalDate newDate = today.plusYears(period.getYears()).plusMonths(period.getMonths()).plusDays(period.getDays());
 
-        Period duration = Period.between(myBirthdate,date);
-        System.out.println();
-        System.out.println(duration.toString());
-        System.out.println();
-        System.out.print("Total number of years: ");
-        System.out.println(duration.getYears());
-        System.out.print("Total number of months: ");
-        System.out.println(duration.getMonths());
-        System.out.print("Total number of days: ");
-        System.out.println(duration.getDays());
-
+        System.out.println(newDate);
 
     }
 }
