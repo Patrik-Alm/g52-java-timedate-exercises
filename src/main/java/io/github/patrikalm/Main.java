4fe88cb67fa2539f87d1fc424480139356ea3eaf
Patrik Alm <almpatrik@yahoo.se>
2024-10-15 11:54:20 +0200

    Time exercise 3

--- src/main/java/io/github/patrikalm/Main.java
+++ src/main/java/io/github/patrikalm/Main.java
@@ -1,8 +1,14 @@
 package io.github.patrikalm;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        LocalDate today = LocalDate.now();
+
+
+        System.out.println(today.format(DateTimeFormatter.ofPattern("eeee dd MMMM")));
     }
 }
