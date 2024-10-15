ff6e6f18f312245c46e7bbb834e70a30027ecb47
Patrik Alm <almpatrik@yahoo.se>
2024-10-15 14:25:17 +0200

    Time exercise 6

--- src/main/java/io/github/patrikalm/Main.java
+++ src/main/java/io/github/patrikalm/Main.java
@@ -2,17 +2,21 @@ package io.github.patrikalm;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.Month;
 import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
 
-        LocalDate myBirthday = LocalDate.parse("1972-02-19");
+        LocalDate date = LocalDate.now();
 
-        DayOfWeek weekday = myBirthday.getDayOfWeek();
+        date = date.plusYears(10);
 
-        System.out.println(weekday);
+        date = date.minusMonths(10);
 
+        Month month = date.getMonth();
+
+        System.out.println(month);
 
     }
 }
