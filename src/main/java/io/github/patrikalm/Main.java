bb49aaf41d6637de723536c5c6480ccecc8de103
Patrik Alm <almpatrik@yahoo.se>
2024-10-15 13:49:59 +0200

    Time exercise 3

--- src/main/java/io/github/patrikalm/Main.java
+++ src/main/java/io/github/patrikalm/Main.java
@@ -6,9 +6,13 @@ import java.time.format.DateTimeFormatter;
 public class Main {
     public static void main(String[] args) {
 
-        LocalDate today = LocalDate.now();
+        LocalDate date = LocalDate.parse("2024-10-14");
 
+        for(int i=0; i<7; i++) {
 
-        System.out.println(today.format(DateTimeFormatter.ofPattern("eeee dd MMMM")));
+        System.out.println(date.format(DateTimeFormatter.ofPattern("eeee")));
+        date = date.plusDays(1);
+
+        }
     }
 }
